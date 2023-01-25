aa79eaa01bfbccbc0f20106cc8597694adbc9193
Noob Science <noobscience123@gmail.com>
2023-01-25 21:18:46 +0530

    Update Structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './components/css/master.css';
 import 'animate.css';
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import Navbar from "./components/navBar";
 import Home from "./components/home";
@@ -9,9 +9,7 @@ function App() {
   return (
       <Router>
           <Navbar />
-          <Routes>
-                <Route exact path="/" element={<Home />}/>
-          </Routes>
+          <Home />
       </Router>
   );
 }
