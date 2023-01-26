247502d1758874b0e9914bd1a2adf5366e9c59e4
Noob Science <noobscience123@gmail.com>
2023-01-26 17:24:18 +0530

    🔗 Add custom Links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './components/css/master.css';
 import 'animate.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Navbar from "./components/navBar";
 import Home from "./components/home";
+import Get from "./components/go"
 
 function App() {
   return (
       <Router>
           <Navbar />
-          <Home />
+          <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/get/:slug" element={<Get />}></Route>
+          </Routes>
       </Router>
   );
 }
---
+++ src/components/go.js
@@ -0,0 +1,28 @@
+import React, { useState} from "react"
+import { useParams, Link } from "react-router-dom";
+import db from "./db/urls.json"
+import "./css/master.css"
+
+const Get = () => {
+    const { slug } = useParams()
+    const getDb = (slug) => {
+        return db[slug]
+    }
+    setTimeout(() => {
+        window.location = getDb(slug)
+    }, 10000)
+    const refresh = () => {
+        window.location.href = "/"
+    }
+    return (
+        <div style={{textAlign: "center"}}>
+            <h1>Site wants to take you to <a href={ getDb(slug) }>{ getDb(slug) }</a></h1>
+            <h1>It will auto redirect in 10 seconds</h1>
+            <div style={{textAlign: "center"}}>
+                <button className="button"><Link to="/" onClick={refresh} className="btn-link">Go Back</Link></button>
+            </div>
+        </div>
+    )
+}
+
+export default Get
