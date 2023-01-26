172ebd284d1acdb154d9de70dfa315475893853b
Noob Science <noobscience123@gmail.com>
2023-01-26 17:54:14 +0530

    ❤️ Update get

--- src/components/go.js
+++ src/components/go.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useParams, Link } from "react-router-dom";
 import db from "./db/urls.json"
 import "./css/master.css"
+import "./css/get.css"
 
 const Get = () => {
     const { slug } = useParams()
@@ -15,9 +16,10 @@ const Get = () => {
         window.location.href = "/"
     }
     return (
-        <div style={{textAlign: "center"}}>
-            <h1>Site wants to take you to <a href={ getDb(slug) }>{ getDb(slug) }</a></h1>
-            <h1>It will auto redirect in 10 seconds</h1>
+        <div className="get-div">
+            <h1>Site wants to take you to</h1>
+            <a href={ getDb(slug) }>{ getDb(slug) }</a>
+            <h1>It will redirect in 10 secs</h1>
             <div style={{textAlign: "center"}}>
                 <button className="button"><Link to="/" onClick={refresh} className="btn-link">Go Back</Link></button>
             </div>
